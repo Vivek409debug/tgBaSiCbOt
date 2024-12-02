63a397ffb9e97d9d38d362b7e4f719d6f1a806bb
VIVEK PS <Viveksubhash5@gmail.com>
2024-12-02 23:15:27 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,15 +5,9 @@ from pyrogram import __version__
 ABOUT_TXT = f"""<b><i><a href='https://github.com/AbOutMeDK/tgBaSiCbOt'>@tgBaSiCbOt</a></b> is an Open Source project made by <a href='https://t.me/DevKrishnaAjith'>Dev Krishna Ajith</a> to make Telegram bot development easier for beginners.\n\nIf you are interested to build a Telegram Bot, this project can help you to build it easily because it's a Simple Repository which you can easily understand and it's simple to modify.</b></i>
 """
 
-OWNER_START_TXT = f"""<i><b>🎊 Congratulations, You successfully builded a Telegram Bot!! 🥳</i></b>
-
-<i>Are you ready to turn your brilliant bot idea into reality? You’ve taken the first step by reading this! Now, let’s turn that vision into a tangible, powerful tool.</i>
-
-<i> <a href='https://t.me/ExamVault'>We're</a></b> here to support you every step of the way, from initial concept to final deployment. Together, we’ll build something amazing.</i>
-
-<b><i><b>Let’s get started!</i></b>
+OWNER_START_TXT = f"""<i><b>Welcome! to ZERO!BOT!* 
+    *I promise to guide you from Zero to Hero in programming! 🚀*</i></b>
 """
-
 NEXT_TXT = f"""<b><i>Now, please forward the initial message received from the bot to @tgBaSiCbOt.</i></b>
 
 <i><b>By forwarding the message you are sending verification request to @tgBaSiCbOt moderators. After reviewing your request they'll connect with you for further update as soon as possible</i></b>
